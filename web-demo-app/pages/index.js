184fe520c50d0b286a3d8760f1a1923250c5d6c4
salimbeni1 <etienne.salimbeni@epfl.ch>
2022-04-26 23:10:22 +0200

    react-tree-fiber demo cube

--- web-demo-app/pages/index.js
+++ web-demo-app/pages/index.js
@@ -2,14 +2,42 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
 
+import { Canvas } from "react-three-fiber";
+import {
+  MeshDistortMaterial,
+  MeshWobbleMaterial,
+  Sphere,
+  OrbitControls
+} from "@react-three/drei";
+
+const get_logo = () => {
+
+  return <Canvas >
+      <ambientLight intensity={1} />
+
+      <directionalLight position={[10, 10, 10]} intensity={10} color="red" />
+      <directionalLight position={[10, -10, 10]} intensity={10} color="blue" />
+        <mesh
+          scale={3}>
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial color='black' />
+        </mesh>
+      <OrbitControls></OrbitControls>
+    </Canvas>
+  
+}
+
+
 export default function Home() {
   return <>
+
+    
   
 
     <div className={styles.header}>
-      <img className={styles.logo} src = "mrviz_logo.svg"  alt="MRViz logo"/>
+      <div className={styles.logo} > {get_logo()} </div>
       <img className={styles.title} src = "mrviz_title.svg" alt="MRViz title"/>
-      <img className={styles.logo} src = "mrviz_logo.svg"  alt="MRViz logo"/>
+      <div className={styles.logo} > {get_logo()} </div>
     </div>
 
 
@@ -19,7 +47,7 @@ export default function Home() {
 
     <div className={styles.infobloc}>
       <div className={styles.paragraph}>
-          <img className={styles.logo} src = "mrviz_logo.svg"  alt="MRViz logo"/>
+          <div className={styles.logo} > {get_logo()} </div>
           <p> Nowadays, engineers developing robotics software have a serious constraint.
               They can visualize the robot s perception of the environment, but cannot have
               a straightforward impression of the difference between such perceived data and
@@ -30,7 +58,7 @@ export default function Home() {
 
 
       <div className={styles.paragraph}>
-          <img className={styles.logo} src = "mrviz_logo.svg"  alt="MRViz logo"/>
+          <div className={styles.logo} > {get_logo()} </div>
           <p> Thanks to our toolkit, which overlays the robot s sensed data onto the real world,
              robotics engineers will benefit from seeing directly what their robot perceives by exploring
               the environment around them with HoloLens. Such feature will simplify the development of ROS 
@@ -41,7 +69,7 @@ export default function Home() {
 
 
       <div className={styles.paragraph}>
-          <img className={styles.logo} src = "mrviz_logo.svg"  alt="MRViz logo"/>
+          <div className={styles.logo} > {get_logo()} </div>
           <p> We plan to visualize one type of messages for the time being, focusing 
             on the other types later. Also, we are considering the possibility of combining together
              the visualizations of several message types, allowing more complex and informative evaluations
@@ -51,7 +79,7 @@ export default function Home() {
 
 
       <div className={styles.paragraph}>
-          <img className={styles.logo} src = "mrviz_logo.svg"  alt="MRViz logo"/>
+          <div className={styles.logo} > {get_logo()} </div>
           <p> Next, we will focus more on visualizing the perceived data, locating HoloLens,
              and tracking robot position.  </p>
       </div>
