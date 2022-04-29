5954ffedc541325a21e522d990284243f54b6328
salimbeni1 <etienne.salimbeni@epfl.ch>
2022-04-29 21:16:57 +0200

    frame models on demo page

--- web-demo-app/pages/index.js
+++ web-demo-app/pages/index.js
@@ -27,12 +27,14 @@ const get_logo = () => {
 
 const get_point = () => {
 
+  return <div style={{height:"10px" , width:"10px", backgroundColor:"#6495ED" , margin:"20px"}}/>
+
   return <Canvas dpr={[1, 2]} orthographic camera={{ position: [-10, 10, 10], zoom: 20 }} >
         <ambientLight intensity={1}/>
         <directionalLight position={[100, 100, 100]} intensity={1} color="white"/>
           
         <Box>
-          <meshStandardMaterial color={'blue'}/>
+          <meshStandardMaterial color={'blue'} />
         </Box>
 
         <OrbitControls />
@@ -63,7 +65,133 @@ export default function Home() {
           
           <directionalLight position={[100, 100, 100]} intensity={1} color="white"/>
           <ambientLight/>
-          <Mrviz_world rotation={[0, 0 ,  0 ]} scale={0.15} />
+          <Mrviz_world angry rotation={[0, 0 ,  0 ]} scale={0.15} />
+
+          <OrbitControls enableZoom={false}/>
+        
+    </Canvas>
+    </div>
+
+    <div className={styles.infobloc}>
+      <div className={styles.paragraph}>
+          <div className={styles.logo} > {get_point()} </div>
+          <p> Nowadays, engineers developing robotics software have a serious constraint.
+              They can visualize the robot s perception of the environment, but cannot have
+              a straightforward impression of the difference between such perceived data and
+              the real physical world. They often have to rely on uninformative visualizations
+              of point clouds or heatmaps on a computer screen with no additional information about
+              the robot s surroundings, making it rather difficult to find the flaws and debug the system.</p>
+      </div>
+
+
+      <div className={styles.paragraph}>
+          <div className={styles.logo} > {get_point()} </div>
+          <p> Thanks to our toolkit, which overlays the robot s sensed data onto the real world,
+             robotics engineers will benefit from seeing directly what their robot perceives by exploring
+              the environment around them with HoloLens. Such feature will simplify the development of ROS 
+              (Robot Operating System) software, allowing engineers to fully leverage sensor data by visualizing
+               them in the mixed reality. </p>
+          
+      </div>
+    
+    </div>
+
+
+
+
+    <div style={{position:"relative" , height:"300px"}}>
+      <Canvas dpr={[1, 2]} orthographic camera={{ position: [200, 80, -100], zoom: 100 }} >
+          
+          <directionalLight position={[100, 100, 100]} intensity={1} color="white"/>
+          <ambientLight/>
+          <Mrviz_world pc wpc phoneMan rotation={[0, 0 ,  0 ]} scale={0.15} />
+
+          <OrbitControls enableZoom={false}/>
+        
+    </Canvas>
+    </div>
+
+    <div className={styles.infobloc}>
+      <div className={styles.paragraph}>
+          <div className={styles.logo} > {get_point()} </div>
+          <p> Nowadays, engineers developing robotics software have a serious constraint.
+              They can visualize the robot s perception of the environment, but cannot have
+              a straightforward impression of the difference between such perceived data and
+              the real physical world. They often have to rely on uninformative visualizations
+              of point clouds or heatmaps on a computer screen with no additional information about
+              the robot s surroundings, making it rather difficult to find the flaws and debug the system.</p>
+      </div>
+
+
+      <div className={styles.paragraph}>
+          <div className={styles.logo} > {get_point()} </div>
+          <p> Thanks to our toolkit, which overlays the robot s sensed data onto the real world,
+             robotics engineers will benefit from seeing directly what their robot perceives by exploring
+              the environment around them with HoloLens. Such feature will simplify the development of ROS 
+              (Robot Operating System) software, allowing engineers to fully leverage sensor data by visualizing
+               them in the mixed reality. </p>
+          
+      </div>
+
+
+      <div className={styles.paragraph}>
+          <div className={styles.logo} > {get_point()} </div>
+          <p> We plan to visualize one type of messages for the time being, focusing 
+            on the other types later. Also, we are considering the possibility of combining together
+             the visualizations of several message types, allowing more complex and informative evaluations
+              of the robot s environment perception. </p>
+          
+      </div>
+
+
+      <div className={styles.paragraph}>
+          <div className={styles.logo} > {get_point()} </div>
+          <p> Next, we will focus more on visualizing the perceived data, locating HoloLens,
+             and tracking robot position.  </p>
+      </div>
+
+
+    </div>
+
+    <div style={{position:"relative" , height:"300px"}}>
+      <Canvas dpr={[1, 2]} orthographic camera={{ position: [200, 80, -100], zoom: 100 }} >
+          
+          <directionalLight position={[100, 100, 100]} intensity={1} color="white"/>
+          <ambientLight/>
+          <Mrviz_world pc base_frame phoneMan rotation={[0, 0 ,  0 ]} scale={0.15} />
+
+          <OrbitControls enableZoom={false}/>
+        
+    </Canvas>
+    </div>
+
+    <div className={styles.infobloc}>
+      <div className={styles.paragraph}>
+          <div className={styles.logo} > {get_point()} </div>
+          <p> Nowadays, engineers developing robotics software have a serious constraint.
+              They can visualize the robot s perception of the environment, but cannot have
+              a straightforward impression of the difference between such perceived data and
+              the real physical world. They often have to rely on uninformative visualizations
+              of point clouds or heatmaps on a computer screen with no additional information about
+              the robot s surroundings, making it rather difficult to find the flaws and debug the system.</p>
+      </div>
+
+
+      <div className={styles.paragraph}>
+          <div className={styles.logo} > {get_point()} </div>
+          <p> Next, we will focus more on visualizing the perceived data, locating HoloLens,
+             and tracking robot position.  </p>
+      </div>
+
+
+    </div>
+
+    <div style={{position:"relative" , height:"300px"}}>
+      <Canvas dpr={[1, 2]} orthographic camera={{ position: [200, 80, -100], zoom: 100 }} >
+          
+          <directionalLight position={[100, 100, 100]} intensity={1} color="white"/>
+          <ambientLight/>
+          <Mrviz_world pc world_frame phoneMan rotation={[0, 0 ,  0 ]} scale={0.15} />
 
           <OrbitControls enableZoom={false}/>
         
